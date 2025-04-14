4d7444a823d2a3a3777dd804b3ba22deaedf0d6a
Mirko Campari <82829377+MaSTERmIKK@users.noreply.github.com>
2025-04-14 10:07:31 +0200

    Add files via upload

---
+++ Esempi _codice/mio_modulo.py
@@ -0,0 +1,7 @@
+x = 10
+
+def somma(z,y):
+    return x + y + z
+
+class Prova():
+    pass
---
+++ Esempi _codice/moduli.py
@@ -0,0 +1,12 @@
+import mio_modulo as mm
+
+print(mm.x)
+
+copia_di_x = mm.x
+nuova_variabile = mm.somma(5,5)
+
+somma = mm.somma(5,6)
+print(somma)
+
+
+Esempio_oggetto = mm.Prova()
